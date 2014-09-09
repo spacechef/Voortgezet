fc6a965aae0f5de2f074396ba471e240badd0501
spacechef <dark_maniac@hotmail.com>
2014-09-09 22:54:12 +0200

    - Comments goed gezet. - java doc werkt nu. - IdentifierInterface toegevoegd.

--- Assignment1/main/CollectionInterface.java
+++ Assignment1/main/CollectionInterface.java
@@ -1,65 +1,67 @@
 package main;
 
+/** Elementen: identifiers van het type Identifier
+ * Structuur: none
+ * Domein: identifiers met minimaal 0 en maximaal 20 identifiers
+ **/
+
 public interface CollectionInterface {
 	
 	static final int MAXIMALE_AANTAL_IDENTIFIERS = 20;
 	static final int MINIMALE_AANTAL_IDENTIFIERS = 0;
 	
-	/* Elementen: identifiers van het type Identifier
-	 * Structuur: none
-	 * Domein: identifiers met minimaal 0 en maximaal 20 identifiers
-	 */
+
 	
-	/* De constructor
-	 * 
-	 * Collectie();
-	 * 
-	 *PRE: -
+	/** constructor
+	 * PRE: -
 	 *POST: Maakt een nieuw collectie object aan.
-	 */
+	 **/
+	void Collection();
 	
-	void copy();
-	/* PRE: -
+	
+	/** PRE: -
 	 * POST: Geeft in een nieuw collectie object een kopie van deze collectie terug.
-	 */
-	
-	void init();
-	/* PRE: - 
+	 **/	
+	void copy();
+
+	/** PRE: - 
 	 * POST: Reset deze collectie naar de oorspronkelijke (leeg) waarden.
-	 */
-	
-	void voegToe();
-	/* PRE: - De collectie bevat minder dan 20 identifiers.
+	 **/	
+	void init();
+
+	/** PRE: - De collectie bevat minder dan 20 identifiers.
 	 * POST: Een kopie van de identifier is toegevoegd aan het einde van collectie
-	 */
-	
-	void lengte();
-	/* PRE: - 
+	 **/	
+	void voegToe();
+
+	/** PRE: - 
 	 * POST: Het aantal identifiers in deze collectie wordt geretourneerd
-	 */
-	
-	void drukAf();
-	/* PRE: - 
+	 **/
+	void lengte();
+
+	/** PRE: - 
 	 * POST: De collectie wordt op het scherm afgedrukt.
-	 */
-	
-	void vereniging(Object collectie);
-	/* PRE: -
+	 **/	
+	void drukAf();
+
+	/** PRE: -
 	 * POST: Geeft een nieuw object terug met daarin de vereniging van deze en de meegegeven collectie
-	 */
+	 **/
+	void vereniging(Object collectie);
 	
-	void verschil(Object collectie);
-	/* PRE: -
+	/** PRE: -
 	 * POST: Geeft een collectie object terug met daarin de elementen die in deze collectie zitten maar niet in de meegegeven collectie.
-	 */
-	
-	void symmetrisch_verschil(Object collectie);
-	/* PRE: - 
+	 **/
+	void verschil(Object collectie);
+
+	/** PRE: - 
 	 * POST: Geeft een nieuw collectie object terug met daarin de elementen die niet in de vereniging van beide collecties zitten.
-	 */
-	
-	void intersectie(Object collectie);
-	/* PRE: -
+	 **/	
+	void symmetrisch_verschil(Object collectie);
+
+	/** PRE: -
 	 * POST: Geeft een nieuw collectie object terug met daarin alle elementen die in beide collecties zitten.
-	 */
+	 **/	
+	void intersectie(Object collectie);
+
 }
--- Assignment1/main/IdentifierInterface.java
+++ Assignment1/main/IdentifierInterface.java
@@ -1,48 +1,51 @@
 package main;
 
+/** Elementen: Alfanumerieke karakters van het type char
+ * Structuur: Linear
+ * Domein: Alle rijen van alfanumerieke karakters die beginnen met een letter en met een minimale lengte van 1.
+ **/
+
 public interface IdentifierInterface {
-	/* Elementen: Alfanumerieke karakters van het type char
-	 * Structuur: Linear
-	 * Domein: Alle rijen van alfanumerieke karakters die beginnen met een letter en met een minimale lengte van 1.
-	 */
-	/* Constructor:
-	 * Identifier();
+
+	/** constructor
 	 * PRE: -
 	 * POST: Heeft een nieuw object van het type Identifier aangemaakt.
-	 */
+	 **/
+	void Identifier();
 	
-	void copy();
-	/* PRE: -
+	/** PRE: -
 	 * POST: Maakt een nieuw object van het type identifier aan met dezelfde waarde als deze identifier
-	 */
-	
-	void init();
-	/* PRE: -
+	 **/	
+	void copy();
+
+	/** PRE: -
 	 * POST: Herstelt deze identifier naar de oorspronkelijke (lege) waarden.
-	 */
-	
-	void voegToe(char c);
-	/* PRE:-
+	 **/	
+	void init();
+
+	/** PRE:-
 	 * POST: Heeft aan het einde van deze identifier een char c toegevoegd
-	 */
-	
-	int lengte();
-	/* PRE: -
+	 **/	
+	void voegToe(char c);
+
+	/** PRE: -
 	 * POST: Retourneert de lengte van deze identifier
-	 */
-	
-	void drukAf();
-	/* PRE: - 
+	 **/	
+	int lengte();
+
+	/** PRE: - 
 	 * POST: Drukt de identifier af.
-	 */
-	
-	boolean isGelijk(Object Identifier);
-	/* PRE: -
+	 **/	
+	void drukAf();
+
+	/** PRE: -
 	 * POST: Geeft aan of de meegegeven identfier gelijk is aan deze identifier
-	 */
-	
-	char get(int i);
-	/* PRE: -
+	 **/	
+	boolean isGelijk(Object Identifier);
+
+	/** PRE: -
 	 * POST: Retourneert char die op de meegegeven index positie staat.
-	 */
+	 **/	
+	char get(int i);
+
 }
