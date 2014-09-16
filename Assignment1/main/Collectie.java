dcc53c8e0bb699db03904a5b6ca74a72bc68ccac
vahidmahmar <vahidmahmar@gmail.com>
2014-09-16 17:46:23 +0200

    Collectie geupdate

--- Assignment1/main/Collectie.java
+++ Assignment1/main/Collectie.java
@@ -1,21 +1,22 @@
 package main;
 
-import java.util.List;
-
 public class Collectie implements CollectieInterface {
+	
+	static final int MAXIMALE_AANTAL_IDENTIFIERS = 20;
+	static final int MINIMALE_AANTAL_IDENTIFIERS = 0;
 	
 	private Identifier[] identifierArray;
 	private Collectie kopie;
-	
-		
-	
+			
 	public Collectie() {
 		Identifier[] identifierArray = new Identifier[0];
 	}
 
-	public Collectie(Collectie bronCollectie) {
+	public Collectie(Collectie teKopierenCollectie) {
 		Collectie kopie = new Collectie();
-		kopie.identifierArray = this.identifierArray; 
+		for(int i = 0; i<teKopierenCollectie.lengte(); i++){
+			kopie.identifierArray[i] = teKopierenCollectie.identifierArray[i];
+		}
 	}
 
 	@Override
@@ -24,33 +25,49 @@ public class Collectie implements CollectieInterface {
 	}
 
 	@Override
-	public void voegToe(Identifier invoerIdentifier) {
-		for(int i=0; i<this.lengte(); i++){
-			if()
-			this.identifierArray = 
-
+	public void voegToe(Identifier invoerIdentifier) throws Exception {	
+		try {
+			if(this.lengte() < MAXIMALE_AANTAL_IDENTIFIERS){
+				Collectie buffer = new Collectie(this);
+				this.identifierArray = new Identifier[this.lengte()+1];
+				for(int i =0; i<this.lengte(); i++){
+					this.identifierArray[i] = new Identifier(buffer.identifierArray[i]);
+				}
+				this.identifierArray[this.lengte()] = new Identifier(invoerIdentifier);
+			}
+			else{
+				throw Exception IndexOutOfBoundsException
+			}
+		}catch(Exception e){
+			System.out.println("De collectie bevat te veel elementen.");
+		}
 	}
 
 	@Override
 	public int lengte() {
 		return this.identifierArray.length;
-		
 	}
 
 	@Override
-	public void verwijder(Identifier i) {
-		
-		
+	public void verwijder(Identifier teVerwijderenIdentifier) {
+		Collectie buffer = new Collectie(this);
+		this.identifierArray = new Identifier[this.lengte()-1];
+		int invoegIndex = 0 ;
+		for(int i = 0; i < this.lengte(); i++){
+			if(buffer.identifierArray[i] != teVerwijderenIdentifier){
+				this.identifierArray[invoegIndex] = new Identifier(buffer.identifierArray[i]);
+				invoegIndex++;
+			}	
+		}			
 	}
 
 	@Override
 	public Identifier pak() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.identifierArray[this.lengte()-1];
 	}
 
 	@Override
-	public Collectie vereniging(Collectie ander) {
+	public Collectie vereniging(Collectie ander) throws Exception {
 		// TODO Auto-generated method stub
 		return null;
 	}
@@ -62,7 +79,7 @@ public class Collectie implements CollectieInterface {
 	}
 
 	@Override
-	public Collectie symmetrisch_verschil(Collectie ander) {
+	public Collectie symmetrisch_verschil(Collectie ander) throws Exception {
 		// TODO Auto-generated method stub
 		return null;
 	}
